4bcefe28d23ca0f462db138fdb4d50b56be03ee1
Gabriel Tomsic <gabriel@awrow.localdomain>
2022-03-18 11:26:33 -0600

    Server is running part 1 and part 2 is all done

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,20 +1,26 @@
+import { Builder, Capabilities, By } from "selenium-webdriver";
 
-import { Builder, Capabilities, By } from "selenium-webdriver"
+require("chromedriver");
 
-require('chromedriver')
-
-const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
-})
+  driver.get("http://localhost:3000/public/index.html");
+});
 
 afterAll(async () => {
-    driver.quit()
-})
+  driver.quit();
+});
 
-test('Title shows up when page loads', async () => {
-    const title = await driver.findElement(By.id('title'))
-    const displayed = await title.isDisplayed()
-    expect(displayed).toBe(true)
-})
+test("Title shows up when page loads", async () => {
+  const title = await driver.findElement(By.id("title"));
+  const displayed = await title.isDisplayed();
+  expect(displayed).toBe(true);
+});
+
+// test("Button chaing the title of button bots", async () => {
+//   await driver
+//     .findElement(By.xpath(`//button[test()="See All Bots"]`))
+//     .sendKeys("See All Bots Not working");
+//   await driver.sleep(5000);
+// });
--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,10 @@
-const {shuffleArray} = require('./utils')
+const { shuffleArray } = require("./utils");
 
-describe('shuffleArray should', () => {
-    // CODE HERE
-})
+// describe("shuffleArray should", () => {
+//   // CODE HERE
+//   test("Testing if one is equal 1", () => {
+//     expect(1).toBe(1);
+//   });
+// });
+
+// console.log(shuffleArray());
