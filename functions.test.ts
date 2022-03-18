c4395db4ecd854940d6b22f6a8884134e232e106
Gabriel Tomsic <gabriel@awrow.local>
2022-03-18 13:54:56 -0600

    Jest Function Checking for the array is working complete

--- functions.test.ts
+++ functions.test.ts
@@ -4,9 +4,9 @@ describe("shuffleArray should", () => {
   // CODE HERE
   const givenShuffle = shuffleArray([1, 2, 3, 4]);
 
-  // test("Checking the length of the array", () => {
-  //   expect(givenShuffle).toHaveBeenCalled([1, 2, 3, 4]);
-  // });
+  test("Checking the length of the array", () => {
+    expect.arrayContaining([1, 2, 3, 4]);
+  });
 
   test("Testing for the length of Array", () => {
     expect(givenShuffle.length).toEqual(4);
