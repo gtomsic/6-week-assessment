171e131ef4b645b81cc91fc713e6c75db721c7f4
Gabriel Tomsic <gabriel@awrow.local>
2022-03-18 21:18:14 -0600

    Sorry being frustrated with this F xpath not working the way so possed to be working couldnt even select a shitty nested element simple

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -20,24 +20,43 @@ test("Title shows up when page loads", async () => {
 
 test("Check that clicking the Draw button displays the div with id = “choices”", async () => {
   await driver.findElement(By.xpath(`//button[@id="draw"]`)).click();
-  await driver.findElement(By.xpath(`//div[@id="choices"]`));
-  await driver.sleep(3000);
+  const classElement = await driver.findElement(
+    By.xpath(
+      `(//div[@id="choices"]/div[@class='bot-card outline' and position()=1]/button[@class="bot-btn"])[1]`
+    )
+  );
+  const isClass = await classElement.isDisplayed();
+  expect(isClass).toBe(true);
+  await driver.sleep(1000);
+
+  /* await driver.findElement(By.xpath(`//button[@id="draw"]`)).click();
+  const classElement = await driver.findElement(
+    By.xpath(`//div[@id="choices"]`)
+  );
+  const isClass = await classElement.isDisplayed();
+  expect(isClass).toBe(true);
+  await driver.sleep(1000);
+  */
 });
 
 test("Check that clicking an “Add to Duo” button displays the div with id = “player-duo”", async () => {
-  // I finally made combination working with foreach
-  await (
-    await driver.findElements(By.xpath(`//button[@class="bot-btn"]`))
-  ).forEach((element) => element.click());
-  await driver.findElement(By.xpath(`//div[@id="player-duo"]`));
-  await driver.sleep(3000);
+  const addToDuo = await driver.findElement(
+    By.xpath(`(//section/div[@id='choices']/div[@class="bot-card outline"])[1]`)
+  );
+  console.log(addToDuo);
+
+  await driver.sleep(1000);
 });
 
-test("Check that when a bot is “Removed from Duo”, that it goes back to “choices””", async () => {
-  const elements = await (
-    await driver.findElements(By.xpath(`//button[text()="Removed from Duo"]`))
-  ).forEach((element) => element.click());
-  console.log(elements);
-  await driver.findElement(By.xpath(`//div[@id="choices"]`));
-  await driver.sleep(3000);
-});
+// test("Check that when a bot is “Removed from Duo”, that it goes back to “choices””", async () => {
+//   // Just to make sure I selected all elements with same class name
+//   // Use the foreach method to attached click in every element
+//   const elements = await (
+//     await driver.findElements(
+//       By.xpath(`(//button[text()="Removed from Duo"])[1]`)
+//     )
+//   ).forEach((element) => element.click());
+//   console.log(elements);
+//   await driver.findElement(By.xpath(`//div[@id="choices"]`));
+//   await driver.sleep(3000);
+// });
